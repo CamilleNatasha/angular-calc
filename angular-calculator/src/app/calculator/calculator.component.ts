4db664d2374cbb816b67b1fa8d301baa3012f02f
CamiN <roanstarsha@gmail.com>
2025-04-26 20:41:25 +0000

    Setting up the calc-template

--- angular-calculator/src/app/calculator/calculator.component.ts
+++ angular-calculator/src/app/calculator/calculator.component.ts
@@ -7,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './calculator.component.scss'
 })
 export class CalculatorComponent {
+  currentInput = '0';
+  firstOperand: number | null = null;
+  operator: string | null = null;
+  waitForSecondNumber = false;
+
+      public getNumber(v: string) {
+        if (this.waitForSecondNumber) {
+          this.currentInput = v;
+          this.waitForSecondNumber = false;
+        } else {
+          this.currentInput === '0' ? this.currentInput = v : this.currentInput += v;
+        }
+      }
 
 }
